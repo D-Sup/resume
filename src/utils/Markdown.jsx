a34ddb2ebf7c95db87e1961dc7881a86e490be52
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-11-04 23:56:17 +0900

    :art: Design: react-markdown 추가 및 커스텀

--- src/utils/Markdown.jsx
+++ src/utils/Markdown.jsx
@@ -3,10 +3,12 @@ import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
 
 export default function Markdown({ children }) {
+  const StringText = ({ children }) => <strong style={{ fontWeight: '700' }}>{children}</strong>
   const ItalicText = ({ children }) => <em style={{ fontStyle: 'inherit' }}><u>{children}</u></em>;
-  const LinkText = ({ children, href }) => <a href={href} style={{ color: '#F99417' }}>{children}</a>;
+  const LinkText = ({ children, href }) => <a href={href} target="_blank" style={{ color: '#F99417' }}>{children}</a>;
 
   const components = {
+    strong: StringText,
     em: ItalicText,
     a: LinkText
   };
