304250e833f251c40aead4cf1a94b187398956de
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-11-04 23:55:48 +0900

    :art: Design: 레이아웃 공통 스타일 추가

---
+++ src/styles/LayoutStyle.jsx
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+const LayoutStyle = createGlobalStyle`
+
+  h2 {
+    font-size: 1.7rem;
+    display: inline;
+    position: relative;
+  &::after {
+    position: absolute;
+    left: 1px;
+    bottom: -15px;
+    content: "";
+    background-color: #000;
+    width: 30%; 
+    height: 4px;
+    z-index: -999;
+  }
+  }
+
+  .grid-container {
+    @media (max-width: 950px) {
+      grid-template-columns: none;
+    }
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+  }
+
+  .child-1,
+  .child-2 {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .child-1 {
+    @media (max-width: 950px) {
+      padding-left: 0;
+    }
+    padding-left: 30px;
+  }
+
+  .child-2 {
+    @media (max-width: 950px) {
+      margin-top: 20px;
+    }
+  }
+`;
+
+export default LayoutStyle;
