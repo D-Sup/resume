5dd9b8bb9bb714347b1f4ab745f61356e31fbbe8
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-11-08 21:55:44 +0900

    :hammer_and_wrench: Feat : App 컴포넌트와 스타일 업데이트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import useFetchData from "./hooks/useFetchData";
+
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+
+import Header from "./components/Header";
+import Info from "./components/Info";
+import Profile from "./components/Profile";
+import Activities from "./components/Activities";
+import Skills from "./components/Skills";
+import EditDataPage from "./components/EditDataPage";
+import Loader from "./components/common/Loader";
 
 function App() {
+
+  const { file, parseData, isLoading, refetchData } = useFetchData("resume");
+  const [isEditPage, setIsEditPage] = useState(false);
+  const [fileData, setFileData] = useState("");
+
+  useEffect(() => {
+    setFileData(file)
+  }, [file])
+
+  if (isLoading) {
+    return <Loader />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WrapperStyle>
+      <GlobalStyle />
+      <Header fileData={fileData} isEditPage={isEditPage} setIsEditPage={setIsEditPage} refetchData={refetchData}/>
+      {isEditPage ? (
+        <EditDataPage fileData={fileData} setFileData={setFileData}/>
+      ) : (
+        <>
+          <Info data={parseData.info} />
+          <Profile data={parseData.Profile} />
+          {
+            parseData.sequence.index.split(' - ').map((item, index) => {
+              if (item === "Skills") {
+                return <Skills key={index} data={parseData[item]} />
+              } else {
+                return <Activities key={index} data={parseData[item]} categoryName={item} />
+              }
+            })
+          }
+        </>
+      )}
+    </WrapperStyle>
   );
 }
 
 export default App;
+
+const WrapperStyle = styled.div`
+  margin: 30px auto;
+  padding: 80px;
+  max-width: 1200px;
+  box-sizing: border-box;
+  width: auto;
+
+  @media (max-width: 950px) {
+    padding: 40px;
+  }
+  section + section {
+    margin-top: 40px;
+  }
+`;
