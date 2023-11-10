1f9d22906bfd30f0ac1b96bbb91798717ab806bd
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-11-10 21:38:53 +0900

    :art: Design: 미디어쿼리 추가

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -100,11 +100,14 @@ const ContactInfoArea = styled.div`
   @media (max-width: 1100px) {
     grid-template-columns: none;
   }
-  
+
   .child-2 {
     @media (max-width: 1100px) {
       padding-left: 30px;
       margin-top: 30px;
+    }
+    @media (max-width: 950px) {
+      padding-left: 0;
     }
   }
   .icon {
